083d28468071831801a9388acc8dab29411d5b5c
kuhn <whfdyer@163.com>
2022-07-10 18:24:42 +0800

    first commit

--- get_miou.py
+++ get_miou.py
@@ -12,46 +12,46 @@ from utils.utils_metrics import compute_mIoU, show_results
 2、该文件计算的是验证集的miou，当前该库将测试集当作验证集使用，不单独划分测试集
 '''
 if __name__ == "__main__":
-    #---------------------------------------------------------------------------#
+    # ---------------------------------------------------------------------------#
     #   miou_mode用于指定该文件运行时计算的内容
     #   miou_mode为0代表整个miou计算流程，包括获得预测结果、计算miou。
     #   miou_mode为1代表仅仅获得预测结果。
     #   miou_mode为2代表仅仅计算miou。
-    #---------------------------------------------------------------------------#
-    miou_mode       = 0
-    #------------------------------#
+    # ---------------------------------------------------------------------------#
+    miou_mode = 0
+    # ------------------------------#
     #   分类个数+1、如2+1
-    #------------------------------#
-    num_classes     = 21
-    #--------------------------------------------#
+    # ------------------------------#
+    num_classes = 21
+    # --------------------------------------------#
     #   区分的种类，和json_to_dataset里面的一样
-    #--------------------------------------------#
-    name_classes    = ["background","aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+    # --------------------------------------------#
+    name_classes = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
     # name_classes    = ["_background_","cat","dog"]
-    #-------------------------------------------------------#
+    # -------------------------------------------------------#
     #   指向VOC数据集所在的文件夹
     #   默认指向根目录下的VOC数据集
-    #-------------------------------------------------------#
-    VOCdevkit_path  = 'VOCdevkit'
+    # -------------------------------------------------------#
+    VOCdevkit_path = 'VOCdevkit'
 
-    image_ids       = open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/val.txt"),'r').read().splitlines() 
-    gt_dir          = os.path.join(VOCdevkit_path, "VOC2007/SegmentationClass/")
-    miou_out_path   = "miou_out"
-    pred_dir        = os.path.join(miou_out_path, 'detection-results')
+    image_ids = open(os.path.join(VOCdevkit_path, "VOC2007/ImageSets/Segmentation/val.txt"), 'r').read().splitlines()
+    gt_dir = os.path.join(VOCdevkit_path, "VOC2007/SegmentationClass/")
+    miou_out_path = "miou_out"
+    pred_dir = os.path.join(miou_out_path, 'detection-results')
 
     if miou_mode == 0 or miou_mode == 1:
         if not os.path.exists(pred_dir):
             os.makedirs(pred_dir)
-            
+
         print("Load model.")
         deeplab = DeeplabV3()
         print("Load model done.")
 
         print("Get predict result.")
         for image_id in tqdm(image_ids):
-            image_path  = os.path.join(VOCdevkit_path, "VOC2007/JPEGImages/"+image_id+".jpg")
-            image       = Image.open(image_path)
-            image       = deeplab.get_miou_png(image)
+            image_path = os.path.join(VOCdevkit_path, "VOC2007/JPEGImages/" + image_id + ".jpg")
+            image = Image.open(image_path)
+            image = deeplab.get_miou_png(image)
             image.save(os.path.join(pred_dir, image_id + ".png"))
         print("Get predict result done.")
 
